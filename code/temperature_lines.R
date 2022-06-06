bd7e53644a60ce60f98713ca52c574e734664618
Süleyman Çite <suleymancite@Suleymans-MacBook-Pro.local>
2022-06-06 11:05:41 +0300

    final version

--- code/temperature_lines.R
+++ code/temperature_lines.R
@@ -9,17 +9,24 @@ t_diff <- read_csv("data/GLB.Ts+dSST.csv", skip = 1, na = "***") %>%
 
 last_dec <- t_diff %>% 
   filter(month == "Dec") %>% 
-  mutate(year = year - 1,
+  mutate(year = year + 1,
          month = "last_Dec")
 next_jan <- t_diff %>% 
   filter(month == "Jan") %>% 
-  mutate(year = year + 1,
+  mutate(year = year - 1,
          month = "next_Jan")
 
-bind_rows(last_dec, t_diff, next_jan) %>%
+t_data <- bind_rows(last_dec, t_diff, next_jan) %>%
   mutate(month = factor(month, levels = c("last_Dec", month.abb, "next_Jan")),
-         month_number = as.numeric(month) - 1) %>%  
-  ggplot(aes(x=month_number, y=t_diff, group=year, color=year)) +
+         month_number = as.numeric(month) - 1,
+         this_year = year == 2022)
+
+t_data %>% 
+  filter(year == 2022)
+
+t_data %>% 
+  ggplot(aes(x=month_number, y=t_diff, group=year, 
+             color=year, size = this_year)) +
   geom_hline(yintercept = 0, color = "white") +
   geom_line() +
   scale_x_continuous(breaks = 1:12,
@@ -27,7 +34,11 @@ bind_rows(last_dec, t_diff, next_jan) %>%
                      sec.axis = dup_axis(name = NULL, labels = NULL)) +
   scale_y_continuous(breaks = seq(-2, 2, 0.2),
                      sec.axis = dup_axis(name = NULL, labels = NULL)) +
-  scale_color_viridis_c() +
+  scale_size_manual(breaks = c(FALSE, TRUE),
+                    values = c(0.25, 1), guide = "none") +
+  scale_color_viridis_c(breaks = seq(1880, 2020, 20),
+                        guide = guide_colorbar(frame.colour = "white",
+                                               frame.linewidth = 1)) +
   coord_cartesian(xlim = c(1,12)) +
   labs(x = NULL,
        y = "Temperature change sine pre-industrial time [\u00B0C]",
@@ -40,7 +51,11 @@ bind_rows(last_dec, t_diff, next_jan) %>%
     axis.ticks = element_line(color = "white", size = 1.5),
     axis.ticks.length = unit(-5, "pt"),
     axis.title = element_text(color = "white", size = 13),
-    plot.title = element_text(colour = "red", hjust = 0.5, size = 18)
+    plot.title = element_text(colour = "red", hjust = 0.5, size = 18),
+    legend.title = element_blank(),
+    legend.background = element_rect(fill = NA),
+    legend.text = element_text(color = "white"),
+    legend.key.height = unit(55, "pt")
   )
   
 ggsave("figures/temperature_lines.png", width = 8, height = 4.5)
